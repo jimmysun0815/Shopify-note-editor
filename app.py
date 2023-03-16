fc23ec8b2542abc97233f69902c1745efe7891d5
Jimmy <jimmy@findora.org>
2023-03-15 19:53:22 -0700

    add  product list

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def edit_order(order_number):
     # Connect to the Shopify store
     token = os.environ.get('SHOPIFY_TOKEN')
     store_url = os.environ.get('SHOPIFY_URL')
-    product_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    product_list = ['CALCITE', 'CARNELIAN', 'AGATE', 'FLUORITE', 'JASPER', 'SODALITE', 'YOOPERLITE', 'AVENTURINE', 'PORKSTONE', 'OPAL', 'LEPIDOLITE', 'AMETHYST', 'MOONSTONE', 'QUARTZ']
     pick_no = 4
     endpoint = f"https://{store_url}/admin/api/2023-01/orders/{order_number}.json"
 
@@ -20,7 +20,8 @@ def edit_order(order_number):
     data = {
         'order': {
             "id": order_number,
-            'note': note + '\n' + ' '.join(str(i) for i in selected_items)
+            'note': note + '\n' + '=================' + '\n' + 'Let your constellation sign guide you, SoulNewMe Al have found those' +' '.join(selected_items) + 'that is a prefect fit you' 
+             
         }
     }  
     put_response = requests.put(endpoint, json=data, headers={"X-Shopify-Access-Token": token})
